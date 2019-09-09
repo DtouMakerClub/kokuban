8449b78387af11f30a1646f49f4945f1f1ee3186
seepfrog <cyadd20@gmail.com>
2019-09-09 20:17:32 +0900

    プロジェクト作成

---
+++ KokubanProject/KokubanProject.cpp
@@ -0,0 +1,12 @@
+﻿#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main()
+{
+	Mat image = Mat::zeros(100, 100, CV_8UC3);
+	imshow("", image);
+	waitKey(1);
+	String x;
+	std::cin >> x;
+}
