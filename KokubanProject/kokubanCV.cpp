b32fd1d8f3a403b83b3e94d30ca48f85226bd0ae
seepfrog <cyadd20@gmail.com>
2020-01-21 03:25:21 +0900

    2値化時の閾値を引数で設定可能に

--- KokubanProject/kokubanCV.cpp
+++ KokubanProject/kokubanCV.cpp
@@ -2,58 +2,33 @@
 
 namespace kokubanCV {
 
-	std::vector<cv::Point>  pulledOutChalkOnKokuban(cv::Mat binary_image)
+	std::vector<cv::Point>  pulledOutChalkOnKokuban(cv::Mat image, int threshold = 128)
 	{
-		//std::vector<std::vector<int>> chalkPoints(binary_image.cols, std::vector<int> (binary_image.rows));
-		//黒板の範囲内を抽出
-		//範囲内だけを走査するか範囲内を切り出してから全体を走査するのどっちが良いかな
-		//cv::Mat kokubanImage = cv::Mat::regionOfImage();
-
-		int width = binary_image.cols;
-		int height = binary_image.rows;
-		//std::vector<std::vector<int>>chalkPoints(width, std::vector<int>(height));
+		int width = image.cols;
+		int height = image.rows;
 		std::vector<cv::Point> chalkPoints;
-
 		cv::Vec3b zero = (0, 0, 0);
-
-		cv::Mat_<cv::Vec3b> img(binary_image);
+		cv::Mat_<cv::Vec3b> img(image);
 
 		for (int y = 0; y <height; ++y) {
 			//y軸を走査
-			cv::Vec3b* src = binary_image.ptr<cv::Vec3b>(y);
-			//cv::Vec3b* p = &img(y, 0);
-			//std::cout << "y = " <<  y <<" :  ";
+			cv::Vec3b* src = image.ptr<cv::Vec3b>(y);
 			for (int x = 0; x < width; ++x) {
 				//x軸を走査
-				if (img.at<cv::Vec3b>(y, x)[0] * 0.299 + img.at<cv::Vec3b>(y, x)[1] * 0.587 + img.at<cv::Vec3b>(y, x)[2] * 0.114 > 128) {
+				if (img.at<cv::Vec3b>(y, x)[0] * 0.299 + img.at<cv::Vec3b>(y, x)[1] * 0.587 + img.at<cv::Vec3b>(y, x)[2] * 0.114 > threshold) {
 					chalkPoints.push_back(cv::Point(x,y));
 					//std::cout << chalkPoints.back() << std::endl;
-					//std::cout << src[x] << std::endl;
 				}
-				//std::cout << src[x];
-				//std::cout << std::endl;
-				//++p;
 			}
 		}
 		std::cout << chalkPoints.size() <<   std::endl;
-		// binary_image.forEach<uchar>([&chalkPoints](uchar& x, const int position[]) -> void {
-		// 	if (x <= 0) {
-		// 		chalkPoints.at(x.x).at(x.y) = 1;
-		// 		std::cout << chalkPoints.at(position[0]).at(position[1]) << std::endl;
-		// 	}
-		// 	});
 
 		//抽出した点群の座標を返す
 		return chalkPoints;
 	}
 
-	/// <summary>
-	/// 入力画像がjpgならまともに動く
-	/// pngは変になる（たぶん配列に入っている順番とか要素が違う）
-	/// </summary>
-	/// <param name="img"></param>
-	/// <returns></returns>
-	cv::Mat binary(cv::Mat img)
+
+	cv::Mat binary(cv::Mat img, int threshold = 128)
 	{
 		int width = img.cols;
 		int height = img.rows;
@@ -65,7 +40,7 @@ namespace kokubanCV {
 
 			for (int x = 0; x < width; ++x) {
 				//x軸を走査
-				if (img.at<cv::Vec3b>(y, x)[0] * 0.299 +  img.at<cv::Vec3b>(y, x)[1] * 0.587 +  img.at<cv::Vec3b>(y, x)[2]  * 0.114 > 128) {
+				if (img.at<cv::Vec3b>(y, x)[0] * 0.299 +  img.at<cv::Vec3b>(y, x)[1] * 0.587 +  img.at<cv::Vec3b>(y, x)[2]  * 0.114 > threshold) {
 					binary_img.at<cv::Vec3b>(y, x)[0] = 255;
 					binary_img.at<cv::Vec3b>(y, x)[1] = 255;
 					binary_img.at<cv::Vec3b>(y, x)[2] = 255;
--- KokubanProject/kokubanCV.h
+++ KokubanProject/kokubanCV.h
@@ -8,10 +8,19 @@ namespace kokubanCV {
 	/// 黒板の外形の座標を貰い黒板の範囲内にある2値化した際に色が残った場所の座標を返す
 	/// </summary>
 	/// <param name="binary_img">2値化した画像</param>
-	/// <param name="kokuban_contours">黒板の外形の頂点座標</param>
+	/// <param name=" threshold">2値化時の閾値</param>
 	/// <returns></returns>
-	std::vector<cv::Point>  pulledOutChalkOnKokuban(cv::Mat binary_image);
-	cv::Mat binary(cv::Mat img);
+	std::vector<cv::Point>  pulledOutChalkOnKokuban(cv::Mat image, int threshold = 128);
+
+/// <summary>
+/// 自作2値か関数
+/// 入力画像がjpgならまともに動く
+/// pngは変になる（たぶん配列に入っている順番とか要素が違う）
+/// </summary>
+/// <param name="img"></param>
+/// <param name=" threshold">2値化時の閾値</param>
+/// <returns></returns>
+	cv::Mat binary(cv::Mat img, int threshold = 128);
 	
 	cv::VideoCapture openVideoFile(std::string video_name);
 	template <class Fn> void funcVideo(std::string video_name, std::string save_file_name, Fn fn);
