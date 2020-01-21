f0b74463c6f7d5145c6d6cb77ab3604904c9552d
seepfrog <cyadd20@gmail.com>
2020-01-21 23:29:48 +0900

    コメント修正

--- KokubanProject/kokubanCV.h
+++ KokubanProject/kokubanCV.h
@@ -5,19 +5,20 @@ namespace kokubanCV {
 
 	/// <summary>
 	/// 黒板上のチョークで書かれた位置を特定
-	/// 黒板の外形の座標を貰い黒板の範囲内にある2値化した際に色が残った場所の座標を返す
+	/// 黒板の外形の座標を貰い黒板の範囲内にある2値化した際に白い場所の座標を返す
 	/// </summary>
-	/// <param name="binary_img">2値化した画像</param>
+	/// <param name="binary_img">カラー画像</param>
 	/// <param name=" threshold">2値化時の閾値</param>
-	/// <returns></returns>
+	/// <returns>白い部分の座標point(x,y)が入ったvector</returns>
 	std::vector<cv::Point>  pulledOutChalkOnKokuban(cv::Mat image, int threshold = 128);
 
 /// <summary>
 /// 自作2値か関数
 /// 入力画像がjpgならまともに動く
+/// mp4もいけた
 /// pngは変になる（たぶん配列に入っている順番とか要素が違う）
 /// </summary>
-/// <param name="img"></param>
+/// <param name="img">カラー画像</param>
 /// <param name=" threshold">2値化時の閾値</param>
 /// <returns></returns>
 	cv::Mat binary(cv::Mat img, int threshold = 128);
